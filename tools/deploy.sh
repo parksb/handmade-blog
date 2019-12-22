21898a803319c934f7e1313de52b313d323b4c6c
ParkSB <parkgds@gmail.com>
2019-12-22 14:14:49 +0900

    feat: Update deploy tool

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 DIST="dist"
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
+
 CYAN="\033[36m"
 GREEN="\033[32m"
 WHITE="\033[0m"
@@ -11,31 +13,29 @@ echo -e "${GREEN}Run build script...${WHITE}"
 echo -e "${WHITE}> ./tools/build.sh"
 ./tools/build.sh
 
-echo -e "${WHITE}> cd ./${DIST}"
-cd ./$DIST
+if [[ $(git status -s) == "" ]]
+then
+  echo -e "${GREEN}Modify gitignore list temporarily...${WHITE}"
+  echo "> sed -i "" "/${DIST}/d" ./.gitignore"
+  sed -i "" "/$DIST/d" ./.gitignore
 
-echo -e "> git status -s\n"
-git status -s
-echo ""
+  echo -e "\n${GREEN}Stage and commit the all files...${WHITE}"
+  echo "> git add ."
+  git add .
+  echo "> git commit -m \"dist: ${DATE}\""
+  git commit -m "dist: $DATE"
 
-echo -e "${GREEN}Stage the all files...${WHITE}"
-echo "> git add ."
-git add .
-echo -e "> git status -s\n"
-git status -s
-echo ""
+  echo -e "\n${GREEN}Use subtree push to gh-pages branch...${WHITE}"
+  echo -e "${WHITE}> cd git push origin \`git subtree split --prefix ${DIST} master\`:gh-pages --force"
+  git push origin `git subtree split --prefix $DIST master`:gh-pages --force
 
-DATE=$(date +"%Y-%m-%d %H:%M:%S")
-while true; do
-  read -p "Do you wish to commit the staged files? (yes/no) " yn
-  case $yn in
-    [Yy]* ) git commit -m "dist: ${DATE}"; break;;
-    [Nn]* ) git reset .; exit;;
-  esac
-done
-
-echo -e "${GREEN}Use subtree push to gh-pages branch...${WHITE}"
-echo -e "${WHITE}> cd ../ && git subtree push --prefix ${DIST} origin gh-pages"
-cd ../ && git subtree push --prefix $DIST origin gh-pages
+  echo -e "\n${GREEN}Reset the temporary changes...${WHITE}"
+  echo -e "${WHITE}> git reset HEAD~"
+  git reset HEAD~
+  echo -e "${WHITE}> git checkout .gitignore"
+  git checkout .gitignore
+else
+  echo "Need clean working directory to deploy."
+fi
 
 echo -e "\n${CYAN}Done!"
