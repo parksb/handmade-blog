1952a10dad324035c915bd079862ff8bba5c5af1
ParkSB <parkgds@gmail.com>
2019-12-29 14:25:49 +0900

    test: Add tests for classes

---
+++ services/tests/Work.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+
+import Work from '../../services/classes/Work';
+
+const workModel = {
+  id: 0,
+  title: 'Lorem Ipsum',
+  subtitle: 'Sed sit amet arcu',
+  thumbnail: 'image.jpg',
+  content: '<p>Lorem ipsum dolor sit amet.</p>',
+};
+
+describe('Work', () => {
+  describe('getWork', () => {
+    it('should returns work model', () => {
+      const work = new Work(workModel);
+      expect(work.getWork()).to.equal(workModel);
+    });
+  });
+
+  describe('getContent', () => {
+    it('should returns only content string', () => {
+      const work = new Work(workModel);
+      expect(work.getContent()).to.equal(workModel.content);
+    });
+  });
+});
